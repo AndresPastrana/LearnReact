9efe453981f40c3555a553c67c11578dfc730986
andresLnx <andreserluis@gmail.com>
2023-09-18 16:45:08 -0400

    core: add lazy load to routes

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,31 +1,18 @@
-import React from 'react'
-import GobackArrow from './components/GobackArrow'
+import React, { lazy, Suspense } from 'react'
 import Link from './components/Link'
 import Route from './components/Route'
 import Router from './components/Router'
-function Landing () {
-  return (
-    <>
-      <h1>Esta es la Landin Page</h1>
-      <GobackArrow />
-    </>
-  )
-}
-function AboutUs () {
-  return <>
-  <h1>Who are we ?</h1>
-   <GobackArrow />
-  </>
-}
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
 const App = () => {
   const router = [
     { path: '/', component: () => <h1>Hola</h1> },
     {
       path: '/home',
-      component: Landing
+      component: Home
     }, {
       path: '/about',
-      component: AboutUs
+      component: About
     }, {
       path: '/search/order/:id',
       component: ({ params }) => <h1>Buscador {JSON.stringify(params)}</h1>
@@ -39,11 +26,14 @@ const App = () => {
       </nav>
 
       <div>
+        <Suspense fallback={<div>Loading....</div>}>
         <Router router={router}>
           <Route path="/otro/path" component={() => <h1>Route as child 1</h1>}/>
           <Route path="/otro/path" component={() => <h1>Route as child 2</h1>}/>
           <h1>Skiped children</h1>
         </Router>
+        </Suspense>
+
       </div>
     </div>
   )
